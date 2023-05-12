d270a77f88ebee6a09d8a38226b81a1d9940f9ff
Michael Gilbert <michael.gilbert@realist.ai>
2023-05-12 17:47:42 +0000

    Refactored test directories. Removed garbage file

--- transpiled_sql/bad_sql.sql
+++
@@ -1,7 +0,0 @@
--- Translation time: 2023-05-12T17:09:52.458964Z
--- Translation job ID: 0d6a16ae-a68d-4c1a-bcfa-71b5a5be187e
--- Source: dwh_preprocessed/bad_sql.sql
--- Translated from: Teradata
--- Translated to: BigQuery
-
--- ERROR_OTHER: Parser failed: After, got bare_identifier(This) but expected [<eof>, tok_semi, tok_lpar, ALTER, BEGIN, BT, CALL, CD, CHECK, CHECKPOINT, CM, COLLECT, COMMENT, COMMIT, CREATE, CT, CV, DATABASE, DEL, DELETE, DIAGNOSTIC, DROP, DUMP, ECHO, ET, EXEC, EXECUTE, EXPLAIN, FLUSH, GIVE, GRANT, HELP, INS, INSERT, LOCK, LOCKING, LOGGING, MERGE, MLOAD, MODIFY, RELEASE, RENAME, REPLACE, RESTART, RESTORE, REVOKE, ROLLBACK, SEL, SELECT, SET, SHOW, UPD, UPDATE, USING, WITH, kw_end(lookahead=TRANSACTION), kw_end(lookahead=FASTEXPORT), kw_end(lookahead=MLOAD), kw_end(lookahead=LOADING), kw_end(lookahead=QUERY), tok_lpar(lookahead=<type>)].
