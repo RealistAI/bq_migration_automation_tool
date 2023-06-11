5f09d620eef90a58189ea769caeeb5dea343ab6c
Ryan den Otter <ryan.denotter@realist.ai>
2023-06-10 22:30:06 -0700

    Moving the csvs to their own dedicated folder. Ensuring consistent quotes are used in the config.py

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ import logging
 #############################################################################
 LOGGING_LEVEL = logging.INFO
 # The CSV File containing a list of UC4 Jobs
-UC4_CSV_FILE = Path(Path.cwd(), "uc4_jobs.csv")
+UC4_CSV_FILE = Path(Path.cwd(), "input_csvs", "uc4_jobs.csv")
 
 # The GCP Project where the metadata will be stored
-METADATA_PROJECT = 'michael-gilbert-dev'
-METADATA_DATASET = 'uc4_conversion_metadata'
+METADATA_PROJECT = "michael-gilbert-dev"
+METADATA_DATASET = "uc4_conversion_metadata"
 
 # The UC4 XML to JSON conversion stores the JSON in a BigQuery table. 
 # This is its name
 UC4_JSON_TABLE = f"{METADATA_PROJECT}.{METADATA_DATASET}.uc4_json"
 
-UC4_SQL_REPO_NAME = 'UC4_SQL'
+UC4_SQL_REPO_NAME = "UC4_SQL"
 
 # Repo containing the SQLS to be translated.
 UC4_SQL_REPO = {
@@ -24,7 +24,7 @@ UC4_SQL_REPO = {
         "branch": "master"
         }
 
-BASE_PATH = Path(Path.home(), 'required_repos')
+BASE_PATH = Path(Path.home(), "required_repos")
 ##############################################################################
 # Generate Teradata to BigQuery Mapping Config                               #
 #############################################################################
@@ -34,8 +34,8 @@ TD_TO_BQ_MAPPING_TABLE = \
         f"{METADATA_PROJECT}.{METADATA_DATASET}.teradata_to_bigquery_mapping"
 
 # A CSV file containing the mapping between business units and datasets
-BUSINESS_UNIT_DATASET_MAP_CSV_FILE = Path(Path.cwd(), 
-                                          'business_unit_dataset_map.csv')
+BUSINESS_UNIT_DATASET_MAP_CSV_FILE = Path(Path.cwd(), "input_csvs",
+                                          "business_unit_dataset_map.csv")
 
 
 ##############################################################################
@@ -59,34 +59,34 @@ BQMS_DEFAULT_DATABASE = "michael-gilbert-dev"
 # Specifies whether or not the BQMS should clean up temp files after running
 BQMS_CLEAN_UP_TEMP_FILES = "True"
 
-BQMS_FOLDER = Path(Path.home(), 'bqms')
+BQMS_FOLDER = Path(Path.home(), "bqms")
 
 # The directory where the BQMS will look for the SQLs that need to be converted
 # NOTE: Everything in this directory will be deleted each time the BQMS is run
-BQMS_INPUT_FOLDER = Path(BQMS_FOLDER, 'input')
+BQMS_INPUT_FOLDER = Path(BQMS_FOLDER, "input")
 
 # This is where the BQMS will write the transpilations to
 # NOTE: Everything in this directory will be deleted each time the BQMS is run
-BQMS_OUTPUT_FOLDER = Path(BQMS_FOLDER, 'output')
+BQMS_OUTPUT_FOLDER = Path(BQMS_FOLDER, "output")
 
 
 # This is where the BQMS will look for the configuraitons
 # NOTE: Everything in this directory will be deleted each time the BQMS is run
-BQMS_CONFIG_FOLDER = Path(BQMS_FOLDER, 'config')
+BQMS_CONFIG_FOLDER = Path(BQMS_FOLDER, "config")
 # This is the main config file used by the BQMS
-BQMS_CONFIG_FILE = Path(BQMS_CONFIG_FOLDER , 'config.yaml')
+BQMS_CONFIG_FILE = Path(BQMS_CONFIG_FOLDER , "config.yaml")
 
 # This file will contain the object mapping created by the Translate SQL script
-BQMS_OBJECT_MAPPING_FILE = Path(BQMS_CONFIG_FOLDER , 'object_mapping.json')
+BQMS_OBJECT_MAPPING_FILE = Path(BQMS_CONFIG_FOLDER , "object_mapping.json")
 
 
 # The Translate SQL script will parse the UC4 Job JSON to determine which 
 # SQLs are referenced and, therefore, which SQLS need to be transpiled. This 
 # is the root directory where the script will look.
 # NOTE: The SQL references in the UC4 jobs have the rest of the file path
-SOURCE_SQL_PATH = Path(BASE_PATH, UC4_SQL_REPO_NAME, 'teradata_sql')
+SOURCE_SQL_PATH = Path(BASE_PATH, UC4_SQL_REPO_NAME, "teradata_sql")
 
 # After successful translaiton and dry-run, the Translate SQL script will copy
 # the SQL files back to this directory.
-TARGET_SQL_PATH = Path(BASE_PATH, UC4_SQL_REPO_NAME, 'bigquery_sql')
+TARGET_SQL_PATH = Path(BASE_PATH, UC4_SQL_REPO_NAME, "bigquery_sql")
 
