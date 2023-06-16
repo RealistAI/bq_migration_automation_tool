ede5aaeb8c858d50d98b817be6be8d708afb1430
Merrik Gilbert <merrik.gilbert@realist.ai>
2023-06-16 21:34:34 +0000

    pushing latest changes to github. it is working at 80%, needs some small changes

--- replace_bind_variables.py
+++ replace_bind_variables.py
@@ -1,4 +1,5 @@
 import re
+import csv
 
 def replace_bind_variables(table_references: list[str]):
     """
@@ -9,47 +10,36 @@ def replace_bind_variables(table_references: list[str]):
 
     #create list that will hold the corrected SQL values with no bind variables.
     improved_table_references = []
-    clean_list_bind_variables = []
+    #where the corrected bind variables will be stored
+    corrected_values = []
 
     #read csv that contains the bind variables and their correct td values.
     with open("bind_variables.csv", "r") as bind_file:
-        bind_variables = bind_file.read()
+        bind_variable_mapping = csv.reader(bind_file, delimiter=",")
+        for bind in bind_variable_mapping:
+            print(bind)
+            print("\n")
 
-    #Iterate through the list of table references and pull out any bind variables with regex.
-    for reference in table_references:
-        print("reference is ", reference)
-        bind_variable_reference = re.findall(r"\$.*?}", reference)
-        if bind_variable_reference == []:
-            continue
-        print("bind reference is ", bind_variable_reference)
-        #Isolate the csv variables from each other and iterate through the reference variables one at a time.
-        for i in range(len(bind_variable_reference)):
-            if type(bind_variables) == type(str()):
-                bind_variables = bind_variables.split("\n")
-            else:
-                pass
-            for i in bind_variables:
-                if i == "":
+            #Iterate through the list of table references and pull out any bind variables with regex.
+            for reference in table_references:
+                matched_bind_variables = re.findall(r"\$.*?}", reference)
+                if matched_bind_variables == []:
                     continue
-                else:
-                    clean_list_bind_variables.append(i)
-            print(clean_list_bind_variables)
 
-            print("file contents are ", bind_variables)
-            print("individual reference ", bind_variable_reference[i])
-            bind_variables_start = bind_variables[i]
-            print("individual variable from file is ", bind_variables_start)
-            #once isolated, take the bind_variables and compare them to the csv values, 
-            #so if there is a match the bind variables will be replaced with their teradata equivalents.
-            if bind_variables_start == " ":
-                continue
-            else:
-                print("first variable in file ", bind_variables_start)
-                if bind_variables_start == bind_variable_reference[i]:
-                    bind_variable = bind_variables[i+i]
-                    improved_table_references.append(bind_variable)
-    print("new list is ", improved_table_references)
+                for bind_variable in matched_bind_variables:
+                    print("iterable is", bind_variable)
+                    print("\n")
 
+                    print("bind mapping value is ", bind[0])
+                    print("\n")
+                    if bind_variable == bind[0]:
+                        corrected_values.append(bind[1])
+
+            print("new list is ", corrected_values)
+            improved_reference = reference.replace("${my-dataset}", str(corrected_values[0]))
+            improved_reference = improved_reference.replace("${my-table}", str(corrected_values[1]))
+            improved_table_references.append(improved_reference)
+        print("improved table reference list is ", improved_table_references)
 
 test_list = ["CREATE TABLE ${my-dataset}.${my-table}", "CREATE TABLE IF NOT EXISTS ${my-table}"]
 replace_bind_variables(table_references=test_list)
